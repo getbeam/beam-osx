315e092ae452af72127cbc8728f49b843bd89fae
plsr <christianpoplawski@gmail.com>
2016-04-27 22:31:14 +0200

    Basic Screenshot Functionality
    Screenshots are taken properly, but still to a default folder (which won't work for anybody with a computer name different from mine)
    
    Also, added a class to create a tmp folder for temporarily saving the screenshot.

--- beam-osx/Screenshot.swift
+++ beam-osx/Screenshot.swift
@@ -10,7 +10,59 @@ import Foundation
 
 class Screenshot {
     
+    let temporaryFolderPath = NSBundle.mainBundle().bundlePath + "/tmp"
+    
     init() {
         debugPrint("Screenshot Initialized!")
+        captureScreenshot()
+    }
+    
+    /// Takes a screenhot using the screencapture command
+    func captureScreenshot() -> NSNumber {
+        
+        /// Set the task
+        let screenshotTask = NSTask()
+        screenshotTask.launchPath = "/usr/sbin/screencapture"
+        screenshotTask.arguments = ["-i", "/Users/Cheese/Desktop/BeamScreenshot.png"]
+        screenshotTask.launch()
+        screenshotTask.waitUntilExit()
+        
+        /// Debug
+        debugPrint("This should not be rpinted until the screenshot task has finished.")
+        
+        createTempoararyFolder()
+        
+        
+        return 0
+    }
+    
+    
+    func createTempoararyFolder() -> Bool {
+        
+        debugPrint("createTempoararyFolder called")
+        
+        // Creates /tmp in bundle if it doesn't yet exist
+        // Should be handled elsewhere
+        let fileManager = NSFileManager()
+        if(!fileManager.fileExistsAtPath(temporaryFolderPath)) {
+            
+            debugPrint("Folder does not exist")
+            
+            do {
+                try fileManager.createDirectoryAtPath(temporaryFolderPath, withIntermediateDirectories: true, attributes: nil)
+                
+                debugPrint("Folder created")
+                
+                return true
+            } catch {
+                print(error)
+                return false
+            }
+            
+        }
+        
+        debugPrint("Folder exists, not created")
+        
+        return false
     }
 }
