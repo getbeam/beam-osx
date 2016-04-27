c12d4673252f0fc772f748a62c8f0365955143dd
plsr <christianpoplawski@gmail.com>
2016-04-27 20:05:40 +0200

    Initial Commit

---
+++ beam-osx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  beam-osx
+//
+//  Created by Christian Poplawski on 27/04/16.
+//  Copyright © 2016 beam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ beam-osx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  beam-osx
+//
+//  Created by Christian Poplawski on 27/04/16.
+//  Copyright © 2016 beam. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
