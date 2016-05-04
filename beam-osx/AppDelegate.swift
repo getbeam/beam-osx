140dd0307863859839df1522ca4739f0ce49582c
plsr <christianpoplawski@gmail.com>
2016-05-04 22:07:33 +0200

    Added Documentation

--- beam-osx/AppDelegate.swift
+++ beam-osx/AppDelegate.swift
@@ -10,19 +10,29 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    // Init StatusBarItem
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
 
     @IBOutlet weak var statusMenu: NSMenu!
     
+    
+    /**
+        Initialises a Screenshot Object when the Screenshot Button in the StatusBar Menu is pressed
+        and thus starts the process to capture a screenshot.
+    */
     @IBAction func takeScreenshotClicked(sender: NSMenuItem) {
         let screenshot = Screenshot()
     }
     
+    
+    /**
+        Terminates the Application when the Quit Menu Item is clicked.
+    */
     @IBAction func quitClicked(sender: NSMenuItem) {
         NSApplication.sharedApplication().terminate(self)
     }
     
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
-
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
@@ -31,9 +41,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     
-    
+    /**
+        Sets the status bar icon up.
+     */
     func setupStatusBarApp() {
-        debugPrint("Entered setupStatusBarApp")
         statusItem.title = "S"
         statusItem.menu = statusMenu
         
